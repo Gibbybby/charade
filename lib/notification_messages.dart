64eeed3ff82c0073deaefae85c6c2c9cd58438ac
David <david@vucak.at>
2025-07-21 22:29:51 +0200

    update

--- lib/notification_messages.dart
+++ lib/notification_messages.dart
@@ -1,12 +1,12 @@
 const List<String> notificationMessages = [
   'Ready for a charade showdown with friends?',
-  'Time to laugh! Gather your crew for CharadeX.',
-  'No party? No problem! Play a round of CharadeX.',
+  'Time to laugh! Gather your crew for Charades.',
+  'No party? No problem! Play a round of Charades.',
   'Challenge accepted! Who can guess the most?',
   'Make tonight memorable with a quick charade game.',
-  'Up for some fun? CharadeX is waiting for you.',
+  'Up for some fun? Charades is waiting for you.',
   'Take a break and enjoy a charade battle!',
   'Get the gang together and start guessing.',
-  'CharadeX brings the party to you!',
+  'Charades brings the party to you!',
   'Ready, set, charade!' 
 ];
